3709f46f7e9621dcf00400fb6021d7bbd4db2c4c
joanall <joana3llaurado@gmail.com>
2021-03-29 23:21:23 +0200

    j

---
+++ registration/projects.php
@@ -0,0 +1,27 @@
+<?php include('server.php')?>
+<?php
+  session_start();
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="projects_style.css">
+  <!-- CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <!-- JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    <?php include('header.html') ?>
--- registration/server.php
+++ registration/server.php
@@ -5,20 +5,15 @@ session_start();
 $username = "";
 $email    = "";
 $table ="";       //result table for search
-
-$keyword = $searchword ="";  //for simple search on index page
+$keyword = $searchword = "";  //for simple search on index page
 $errors = array();
-$samplename = $celltype = $idfreezer = $rack = $position = $amount = $frozendate = $availability = $comment = $Location = $Contact_email = $Full_name = $Contact_phone = $Position = $Main_task = $Find_me = $Institute = '';
-$errors_registration = array('username' => '', 'email' => '', 'password_1' => '', 'password_2' => '', 'fullname' => '', 'cemail' => '', 'cphone' => '');
-// connect to the database
-$db = mysqli_connect('localhost', 'albert', '/Puiyuaru1616', 'mydb');
 
 $idSample = $samplename = $celltype = $idfreezer = $rack = $position = $amount = $frozendate = $availability = $idOwner = $comment = '';
 $idStorage = $storagename = $location = "";
-
+$errors_registration = array('username' => '', 'email' => '', 'password_1' => '', 'password_2' => '');
 // connect to the database
 # $db = mysqli_connect('localhost', 'albert', '/Puiyuaru1616', 'mydb');   # albert pw
-// $db = mysqli_connect('localhost', 'tidytubes', 'Welcome123%', 'mydb');    # jo & lili pw
+$db = mysqli_connect('localhost', 'tidytubes', 'Welcome123%', 'mydb');    # jo & lili pw
 
 
 // NEW SEARCH
@@ -73,8 +68,6 @@ $idStorage = $storagename = $location = "";
     //}
     //echo "</table>";
   //}
-
-
 
 
 
